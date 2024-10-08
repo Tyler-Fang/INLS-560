d41489757864a25a3a021c97bf46619f6cd3e47c
Tyler-Fang <ftyler@ad.unc.edu>
2024-10-07 20:04:48 -0400

    Add files via upload

---
+++ assignment_03/cars_v2.py
@@ -0,0 +1,17 @@
+# Assignment 3
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.capitalize())
+
+# Changing it to what I want
+sports = ['rock climbing', 'snowboarding', 'surfing', 'biking']
+
+for sport in sports:
+    if sport == "surfing":
+        print(sport.title())
+    else:
+        print(sport.upper())
